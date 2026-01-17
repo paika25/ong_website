a32913f95768a9f4623357658ed4bd0841352ea1
Marius <mariustsiorimbola@gmail.com>
2026-01-17 08:37:33 +0300

    add pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,8 @@ export default defineNuxtConfig({
   
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL || '',
-      supabaseKey: process.env.NUXT_PUBLIC_SUPABASE_KEY || '',
+      supabaseUrl: '',
+      supabaseKey: '',
     }
   },
   
